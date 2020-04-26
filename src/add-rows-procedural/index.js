8ecea98a1cb4a1602478181f969401475c435d73
Aaron Cohen <acohen@mountainpasstech.com>
2020-04-26 18:10:34 +0300

    add code

--- src/add-rows-procedural/index.js
+++ src/add-rows-procedural/index.js
@@ -2,14 +2,14 @@ const fs = require("fs");
 
 function addData(path) {
   let ret = 0;
-  const data = fs.readFileSync(path, {encoding: "utf8"});
+  const data = fs.readFileSync(path, { encoding: "utf8" });
   // error checking with file system read etc.
   for (const line of data.split("\n")) {
-   if (line !== "") {
-    const [x, y] = line.split(",");
-    ret += parseInt(x) + parseInt(y);  
-   }
-  return ret;
+    if (line !== "") {
+      const [x, y] = line.split(",");
+      ret += parseInt(x) + parseInt(y);
+    }
+    return ret;
   }
 }
 console.log(addData("./data.csv"));
