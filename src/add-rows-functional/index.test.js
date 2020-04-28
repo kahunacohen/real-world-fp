3a3d233f544278c0851eee516acf311988dd63e3
Aaron Cohen <acohen@mountainpasstech.com>
2020-04-28 12:53:35 +0300

    checkpoint

--- src/add-rows-functional/index.test.js
+++ src/add-rows-functional/index.test.js
@@ -1,13 +1,14 @@
 const fs = require("fs");
 
 const addData = s => {
-  return s.split("\n").reduce((acc1, line) => acc1 + line.split(",").reduce((acc, col) => parseInt(acc) + parseInt(col)), 0)
+  return s.split("\n")
+    .reduce((lineAcc, line) => lineAcc + line.split(",")
+      .reduce((colAcc, col) => parseInt(colAcc) + parseInt(col)), 0)
 }
 
 describe("addData", () => {
   it("adds rows of numbers", () => {
-    const s = "1,2\n100,0\n1,1";
-    //expect(addData(s)).toEqual(105);
-    console.log(addData(s));
+    const s = "1,2,3\n100,0,1\n1,1";
+    expect(addData(s)).toEqual(109);
   });
 });
