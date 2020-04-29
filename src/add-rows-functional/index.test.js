91c4b43054aad19ca63c70b60f5ffe030193c3c4
Aaron Cohen <acohen@mountainpasstech.com>
2020-04-29 21:12:43 +0300

    make new funciton

--- src/add-rows-functional/index.test.js
+++ src/add-rows-functional/index.test.js
@@ -1,14 +1,62 @@
 const fs = require("fs");
 
-const addData = s => {
-  return s.split("\n")
-    .reduce((lineAcc, line) => lineAcc + line.split(",")
-      .reduce((colAcc, col) => parseInt(colAcc) + parseInt(col)), 0)
-}
+const yearlySalariesAsCSV = (xs) => {
+  return (
+    "Last Name,First Name,Total Payments\n" +
+    xs
+      .map(
+        (x) =>
+          `${x.lastName},${x.firstName},${x.pay.reduce(
+            (acc, curr) => acc + curr,
+            0
+          )}`
+      )
+      .join("\n")
+  );
+};
 
 describe("addData", () => {
   it("adds rows of numbers", () => {
-    const s = "1,2,3\n100,0,1\n1,1";
-    expect(addData(s)).toEqual(109);
+    data = [
+      {
+        firstName: "John",
+        lastName: "Doe",
+        pay: [
+          8333.33,
+          8333.33,
+          8333.33,
+          8021.45,
+          7023.0,
+          9023.67,
+          8333.33,
+          8333.33,
+          8333.33,
+          6500.0,
+          8333.33,
+          8333.33,
+        ],
+      },
+      {
+        firstName: "Mary",
+        lastName: "Jane",
+        pay: [
+          12083.33,
+          12083.33,
+          12083.33,
+          11000.0,
+          12102.24,
+          12083.33,
+          12083.33,
+          12083.33,
+          20076.0,
+          12083.33,
+          12083.33,
+          12083.33,
+        ],
+      },
+    ];
+    expect(yearlySalariesAsCSV(data)).toEqual(
+      "Last Name,First Name,Total Payments\nDoe,John,97234.76\nJane,Mary,151928.21"
+    );
   });
 });
--- src/add-rows-procedural/index.test.js
+++ src/add-rows-procedural/index.test.js
@@ -4,7 +4,7 @@ function addData(path) {
   let ret = 0;
   const data = fs.readFileSync(path, { encoding: "utf8" });
   const lines = data.split("\n");
-  for (let i = 0; i < lines.length; i++) {;
+  for (let i = 0; i < lines.length; i++) {
     const cols = lines[i].split(",");
     for (let j = 0; j < cols.length; j++) {
       ret += parseInt(cols[j]);
