fe00ae1ebde6e6745ac3c56293cc3630e61bd606
Aaron Cohen <acohen@mountainpasstech.com>
2020-04-30 15:37:59 +0300

    clean up

--- src/add-rows-procedural/index.test.js
+++ src/add-rows-procedural/index.test.js
@@ -1,20 +1,55 @@
 const fs = require("fs");
 
-function addData(path) {
-  let ret = 0;
-  const data = fs.readFileSync(path, { encoding: "utf8" });
-  const lines = data.split("\n");
-  for (let i = 0; i < lines.length; i++) {
-    const cols = lines[i].split(",");
-    for (let j = 0; j < cols.length; j++) {
-      ret += parseInt(cols[j]);
-    }
+class SalaryManager {
+  constructor(inPath, outPath) {
+    // Ignore possible errors.
+    this.data = JSON.parse(fs.readFileSync(inPath, { encoding: "utf-8" }));
+    this.outPath = outPath;
   }
-  return ret;
+  writeReport() {
+    // The first row of the return array are the headers
+
+    let ret = [["Last Name", "First Name"]];
+
+    // For each employee...
+    for (let i = 0; i < this.data.length; i++) {
+      const employee = this.data[i];
+      let employeeTotal = 0;
+
+      // Sum the yearly payments
+      for (let j = 0; j < employee.pay.length; j++) {
+        employeeTotal += employee.pay[j];
+      }
+
+      // Add a row with the employee's info, including total salary
+      let row = [employee.lastName, employee.firstName, employeeTotal];
+      ret.push(row);
+    }
+    fs.writeFileSync(this.outPath, ret.join("\n"), { encoding: "utf-8" });
+  }
 }
 
-describe("addData", () => {
-  it("sums all the rows of arbitrary column of numbers", () => {
-    expect(addData(`${__dirname}/data.csv`)).toEqual(109);
+describe("Salary Manager", () => {
+  const outPath = `${__dirname}/report.csv`;
+  const safeDelete = () => {
+    if (fs.existsSync(outPath)) {
+      fs.unlinkSync(outPath);
+    }
+  };
+  beforeEach(() => {
+    safeDelete();
+  });
+  afterEach(() => {
+    safeDelete();
+  });
+  it("writeReport writes a correct CSV file", () => {
+    const salaryReporter = new SalaryManager(
+      `${__dirname}/employees.json`,
+      outPath
+    );
+    salaryReporter.writeReport();
+    expect(fs.readFileSync(outPath, { encoding: "utf-8" })).toEqual(
+      "Last Name,First Name\nDoe,John,97234.76\nJane,Mary,151928.21"
+    );
   });
 });
