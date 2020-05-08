3a34e6f9c30b13d31280fddeba5cbe9cf5b00071
Aaron Cohen <acohen@mountainpasstech.com>
2020-05-08 12:49:58 +0300

    add test

--- src/add-rows-functional/index.test.js
+++ src/add-rows-functional/index.test.js
@@ -5,49 +5,6 @@ const { compose } = require("ramda");
 const readFile = (p) => fs.readFileSync(p, { encoding: "utf-8" });
 const parseJSONFile = compose(JSON.parse, readFile);
 
-describe("parseJSONFile", () => {
-  it("gets a JSON string from a file and parses it", () => {
-    expect(parseJSONFile(`${__dirname}/employees.json`)).toEqual([
-      {
-        firstName: "John",
-        lastName: "Doe",
-        pay: [
-          8333.33,
-          8333.33,
-          8333.33,
-          8021.45,
-          7023,
-          9023.67,
-          8333.33,
-          8333.33,
-          8333.33,
-          6500,
-          8333.33,
-          8333.33,
-        ],
-      },
-      {
-        firstName: "Mary",
-        lastName: "Jane",
-        pay: [
-          12083.33,
-          12083.33,
-          12083.33,
-          11000,
-          12102.24,
-          12083.33,
-          12083.33,
-          12083.33,
-          20076,
-          12083.33,
-          12083.33,
-          12083.33,
-        ],
-      },
-    ]);
-  });
-});
-
 const makeTable = (employees) => {
   return [["Last Name", "First Name"]].concat(
     employees.map((e) => [
@@ -57,46 +14,98 @@ const makeTable = (employees) => {
     ])
   );
 };
-console.log(
-  makeTable([
-    {
-      firstName: "John",
-      lastName: "Doe",
-      pay: [
-        8333.33,
-        8333.33,
-        8333.33,
-        8021.45,
-        7023,
-        9023.67,
-        8333.33,
-        8333.33,
-        8333.33,
-        6500,
-        8333.33,
-        8333.33,
-      ],
-    },
-    {
-      firstName: "Mary",
-      lastName: "Jane",
-      pay: [
-        12083.33,
-        12083.33,
-        12083.33,
-        11000,
-        12102.24,
-        12083.33,
-        12083.33,
-        12083.33,
-        20076,
-        12083.33,
-        12083.33,
-        12083.33,
-      ],
-    },
-  ])
-);
+
+describe("program", () => {
+  describe("parseJSONFile", () => {
+    it("gets a JSON string from a file and parses it", () => {
+      expect(parseJSONFile(`${__dirname}/employees.json`)).toEqual([
+        {
+          firstName: "John",
+          lastName: "Doe",
+          pay: [
+            8333.33,
+            8333.33,
+            8333.33,
+            8021.45,
+            7023,
+            9023.67,
+            8333.33,
+            8333.33,
+            8333.33,
+            6500,
+            8333.33,
+            8333.33,
+          ],
+        },
+        {
+          firstName: "Mary",
+          lastName: "Jane",
+          pay: [
+            12083.33,
+            12083.33,
+            12083.33,
+            11000,
+            12102.24,
+            12083.33,
+            12083.33,
+            12083.33,
+            20076,
+            12083.33,
+            12083.33,
+            12083.33,
+          ],
+        },
+      ]);
+    });
+  });
+  describe("makeTable", () => {
+    expect(
+      makeTable([
+        {
+          firstName: "John",
+          lastName: "Doe",
+          pay: [
+            8333.33,
+            8333.33,
+            8333.33,
+            8021.45,
+            7023,
+            9023.67,
+            8333.33,
+            8333.33,
+            8333.33,
+            6500,
+            8333.33,
+            8333.33,
+          ],
+        },
+        {
+          firstName: "Mary",
+          lastName: "Jane",
+          pay: [
+            12083.33,
+            12083.33,
+            12083.33,
+            11000,
+            12102.24,
+            12083.33,
+            12083.33,
+            12083.33,
+            20076,
+            12083.33,
+            12083.33,
+            12083.33,
+          ],
+        },
+      ])
+    ).toEqual([
+      ["Last Name", "First Name"],
+      ["Doe", "John", 97234.76],
+      ["Jane", "Mary", 151928.21],
+    ]);
+  });
+});
+
 // const getEmployees = (path) => fs.readFileSync(path, { encoding: "utf-8" });
 
 // const yearlySalariesAsCSV = (xs) => {
