a900acf5361e3c639fb8b4d4a79ea67cac9f58d3
Aaron Cohen <acohen@mountainpasstech.com>
2020-06-07 21:33:29 +0300

    html passes

--- src/salary-reporter-take2.test.js
+++ src/salary-reporter-take2.test.js
@@ -1,5 +1,6 @@
-const { compose, filter } = require("ramda");
+const { compose, filter, join } = require("ramda");
 const fs = require("fs");
+const path = require("path");
 
 const censor = (s) =>
   s.replace(/\d{3}-\d{2}-(\d{4})/g, (_, lastFour) => `xxx-xx-${lastFour}`);
@@ -16,32 +17,71 @@ const sortByLastName = (xs) => {
   });
 };
 
-const makeTable = (employees) => {
-  return [["Last Name", "First Name"]].concat(
+const JSONtoTable = (employees) => {
+  return [
+    ["Last Name", "First Name", "Total Pay", "Social Security Number"],
+  ].concat(
     employees.map((x) => [
       x.lastName,
       x.firstName,
       x.pay.reduce((acc, curr) => acc + curr),
+      x.socialSecurity,
     ])
   );
 };
 
-const f = compose(
-  makeTable,
+const employeeJSONToTable = compose(
+  JSONtoTable,
   sortByLastName,
   filter((x) => x.active),
   JSON.parse,
   censor
 );
 
-describe("foo", () => {
+const employeesToCSV = compose(join("\n"), employeeJSONToTable);
+
+const toHTML = (tableData) => {
+  const headerRow =
+    "<tr>" +
+    tableData[0].map((heading) => `<th>${heading}</th>`).join("") +
+    "</tr>";
+
+  const dataRows = tableData
+    .slice(1) // Everything but the first row
+    .map((row) => {
+      const cells = row.map((data) => `<td>${data}</td>`).join("");
+      return `<tr>${cells}</tr>`;
+    })
+    .join("");
+
+  return `
+  <table>
+    <thead>
+      ${headerRow}
+    </thead>
+    <tbody>
+      ${dataRows}
+    </tbody>
+  </table>`;
+};
+
+const employeesToHTML = compose(toHTML, employeeJSONToTable);
+
+describe("employees", () => {
   let employeesStr;
   beforeAll(() => {
     employeesStr = fs.readFileSync(`${__dirname}/employees.json`, {
       encoding: "utf-8",
     });
   });
-  it("does", () => {
-    console.log(f(employeesStr));
+  it("toCSV", () => {
+    expect(employeesToCSV(employeesStr)).toEqual(
+      "Last Name,First Name,Total Pay,Social Security Number\nDoe,John,97234.76,xxx-xx-2588\nJane,Mary,151928.21,xxx-xx-6322"
+    );
+  });
+  it("toHTML", () => {
+    expect(employeesToHTML(employeesStr)).toContain(
+      "<tr><td>Doe</td><td>John</td><td>97234.76</td><td>xxx-xx-2588</td></tr><tr><td>Jane</td><td>Mary</td><td>151928.21</td><td>xxx-xx-6322</td></tr>"
+    );
   });
 });
